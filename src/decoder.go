6afb9ab0e6a0c91547e716901185b76ea8703c20
skyitachi <skbysp@gmail.com>
2017-08-28 19:29:43 +0800

    feat: add readString

--- src/decoder.go
+++ src/decoder.go
@@ -4,6 +4,7 @@ import (
   "bytes"
   "github.com/pkg/errors"
 )
+
 type Decoder struct {
   buf *bytes.Buffer
 }
@@ -20,7 +21,24 @@ func (decoder *Decoder) readn(n int) []byte {
   return decoder.buf.Next(n)
 }
 
-func (decoder *Decoder) readInt() (int, error) {
+func (decoder *Decoder) read_n_rune(n int) string {
+  var ret []rune
+  for {
+    r, _, err := decoder.buf.ReadRune()
+    if err != nil {
+      decoder.buf.UnreadRune()
+      break
+    }
+    ret = append(ret, r)
+    n--
+    if n <= 0 {
+      break
+    }
+  }
+  return string(ret)
+}
+
+func (decoder *Decoder) ReadInt() (int, error) {
   code, err := decoder.read()
   if err != nil {
     return 0, err
@@ -53,7 +71,7 @@ func (decoder *Decoder) readInt() (int, error) {
   return 0, errors.New("readInt error: " + string(code))
 }
 
-func (decoder Decoder) readBoolean() (bool, error) {
+func (decoder Decoder) ReadBoolean() (bool, error) {
   code, err := decoder.read()
   if err != nil {
     return false, err
@@ -67,3 +85,47 @@ func (decoder Decoder) readBoolean() (bool, error) {
     return false, errors.New("readBoolean error: unexpected code, 'T' or 'F' expected")
   }
 }
+
+/**
+ * string ::= x52 b1 b0 <utf8-data> string
+ *        ::= S b1 b0 <utf8-data>
+ *        ::= [x00-x1f] <utf8-data>
+ *        ::= [x30-x33] b0 <utf8-data>
+ */
+func (decoder Decoder) ReadString() (string, error) {
+  code, err := decoder.read()
+  if err != nil {
+    return "", err
+  }
+  switch {
+  case code == 0x53 || code == 0x52:
+    bits := decoder.readn(2)
+    size := int(bits[0]<<8 + bits[1])
+    ret := decoder.read_n_rune(size)
+    if len(ret) < size {
+      return "", errors.New("readString error: unexpected length")
+    }
+    return ret, nil
+  case code >= 0x00 && code <= 0x1f:
+    size := int(code)
+    ret := decoder.read_n_rune(size)
+    if len(ret) < size {
+      return "", errors.New("readString error: unexpected length")
+    }
+    return ret, nil
+  case code >= 0x30 && code <= 0x33:
+    bits := decoder.readn(1)
+    if len(bits) < 1 {
+      return "", errors.New("readString error: unexpected length")
+    }
+    size := int(code + bits[0])
+    ret := decoder.read_n_rune(size)
+    if len(ret) < size {
+      return "", errors.New("readString error: unexpected length")
+    }
+    return ret, nil
+  default:
+    return "", errors.New("readString error: unexpected code")
+  }
+}
+
--- src/decoder_test.go
+++ src/decoder_test.go
@@ -1,6 +1,10 @@
 package hessian
 
-import "testing"
+import (
+  "testing"
+  "fmt"
+  "strings"
+)
 
 func unexpected_error(err error, t* testing.T) {
   if err != nil {
@@ -11,7 +15,7 @@ func unexpected_error(err error, t* testing.T) {
 func TestReadInt(t *testing.T) {
   code := []byte{0x49, 0x00, 0x00, 0x00, 0x01}
   decoder := NewDecoder(code)
-  n, err := decoder.readInt()
+  n, err := decoder.ReadInt()
   unexpected_error(err, t)
   if n != 1 {
     t.Errorf("bytes: 4900000001 should be decoded to 1")
@@ -21,18 +25,38 @@ func TestReadInt(t *testing.T) {
 func TestReadBoolean(t *testing.T) {
   code := []byte{0x54, 0x46, 0x47}
   decoder := NewDecoder(code)
-  r, err := decoder.readBoolean()
+  r, err := decoder.ReadBoolean()
   unexpected_error(err, t)
   if r != true {
     t.Errorf("byte 0x54 should be True")
   }
-  r, err = decoder.readBoolean()
+  r, err = decoder.ReadBoolean()
   unexpected_error(err, t)
   if r != false {
     t.Errorf("byte 0x46 should be False")
   }
-  _, err = decoder.readBoolean()
+  _, err = decoder.ReadBoolean()
   if err == nil {
     t.Errorf("readBoolean should reture error")
+  }
+}
+
+func TestReadString(t *testing.T) {
+  code := []byte{0x05, 0x68, 0x65, 0x6c, 0x6c, 0x6f}
+  decoder := NewDecoder(code)
+  s, err := decoder.ReadString()
+  unexpected_error(err, t)
+  fmt.Printf("ans is %s, size is %d\n", s, len(s))
+  if strings.Compare(s,"hello") != 0 {
+    t.Errorf("readString: decoder error")
+  }
+  // utf_8
+  code = []byte{0x02, 0xe4, 0xbd, 0xa0, 0xe5, 0xa5, 0xbd}
+  decoder = NewDecoder(code)
+  s, err = decoder.ReadString()
+  unexpected_error(err, t)
+  fmt.Println(len(s))
+  if strings.Compare(s, "你好") != 0 {
+    t.Errorf("readString: decoder error")
   }
 }
